3b1a0c975d10b9e96c251d1c2a2718129e29f127
milanbargiel <milan.bargiel@googlemail.com>
2020-04-06 15:56:13 +0200

    add lazyloading to images

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,6 @@
-console.log('kokos bananae');
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+    img.removeAttribute('data-src');
+  };
+});
