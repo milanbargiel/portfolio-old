69867cf996feb2d26a0d062a785ffd64a4ff2009
milanbargiel <milan.bargiel@googlemail.com>
2020-04-08 15:04:34 +0200

    optimize for retina

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,10 @@
-[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+const images = document.querySelectorAll('img[data-src][data-srcset]');
+images.forEach((img) => {
   img.setAttribute('src', img.getAttribute('data-src'));
-  img.onload = function() {
+  img.setAttribute('srcset', img.getAttribute('data-srcset'));
+  // when image is loaded remove data attribute to make image fade in with a css animation
+  img.addEventListener('load', () => {
     img.removeAttribute('data-src');
-  };
+    img.removeAttribute('data-srcset');
+  });
 });
