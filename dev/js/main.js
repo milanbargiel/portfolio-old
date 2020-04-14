52b78ae2a7cb13129b9b5cc0317ca7268fd2e725
milanbargiel <milan.bargiel@googlemail.com>
2020-04-14 15:30:33 +0200

    add font into svg

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,15 @@
-const images = document.querySelectorAll('img[data-src][data-srcset]');
+const images = document.querySelectorAll('img[data-src]');
 images.forEach((img) => {
+  const hasSrcSet = img.getAttribute('data-srcset');
   img.setAttribute('src', img.getAttribute('data-src'));
-  img.setAttribute('srcset', img.getAttribute('data-srcset'));
+  if (hasSrcSet) {
+    img.setAttribute('srcset', img.getAttribute('data-srcset'));
+  }
   // when image is loaded remove data attribute to make image fade in with a css animation
   img.addEventListener('load', () => {
     img.removeAttribute('data-src');
-    img.removeAttribute('data-srcset');
+    if (hasSrcSet) {
+      img.removeAttribute('data-srcset');
+    }
   });
 });
